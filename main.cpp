c0dd72c27d224b4087c7b699bf66efdb99dce6a5
hiromi12 <hhonda120901@gmail.com>
2023-10-12 16:41:04 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+// Binary Search Tree
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+  int data;
+  node *p, *l, *r;  // parent, left, right pointer
+  node(int d) {
+    data = d;
+    p = l = r = NULL;
+  };
+};
+
+class BST: public node {
+public:
+  void Insert(int data);
+  void Delete(int data);
+  void inorderTraversal();
+  int Height();
+};
+
+void BST::Insert(int data) {
+
+}
+void BST::Delete(int data) {
+
+}
+void BST::inorderTraversal() {
+
+}
+int BST::Height() {
+  int height;
+  return height;
+}
+
+int main() {
+  return 0;
+}
