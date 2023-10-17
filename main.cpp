cec90d3ed5d9ee46cb1b38add0be44b4381e6114
hiromi12 <hhonda120901@gmail.com>
2023-10-16 22:10:31 -0500

    some comments added

--- main.cpp
+++ main.cpp
@@ -62,6 +62,11 @@ void BST::Insert(int data) {
   }
 }
 
+/**
+ * @brief The InorderTraversal function performs an in-order traversal of the BST starting from a given node. In-order traversal is a depth-first traversal method where the nodes are visited in the following order: left subtree, current node, right subtree. For a BST, this traversal method results in visiting the nodes in ascending order.
+ * 
+ * @param node 
+ */
 void BST::InorderTraversal(Node *node) {
   if (node == NULL)
     return;
@@ -70,9 +75,19 @@ void BST::InorderTraversal(Node *node) {
   InorderTraversal(node->right);
 }
 
+/**
+ * @brief The Height function is a recursive function that calculates the height of a BST starting from a given node. The height of a node in a BST is defined as the length of the longest path from that node to a leaf node. In other words, it measures how far away the deepest leaf node is from the given node.
+ * 
+ * @param node 
+ * @return int 
+ */
 int BST::Height(Node *node) {
-  if (node == NULL)
+  if (node == NULL)   // Best case
     return -1;
+  /**
+   * Recursive Step:
+   * The function calculates the height of the left and right subtrees of the current node recursively. The height of the current node is then determined by taking the maximum of the heights of its left and right subtrees, max(leftHeight, rightHeight), and adding 1 to it. The +1 accounts for the current node itself.
+   */
   int leftHeight = Height(node->left);
   int rightHeight = Height(node->right);
   return max(leftHeight, rightHeight) + 1;
